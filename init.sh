0651b71a87f1f2ccd16e364d850c21cdf9c40804
Brian Rainey <brian@rainey.us>
2017-01-02 20:11:08 -0600

    Git config tweaks.

--- init.sh
+++ init.sh
@@ -12,6 +12,8 @@ sudo apt install -y git stow emacs-nox vim-nox tmux
 
 git config --global user.name "$1"
 git config --global user.email "$2"
+git config --global push.default simple
+git config --global core.editor vim
 
 git clone https://github.com/brianrainey/.emacs.d.git
 git clone https://github.com/brianrainey/.dotfiles.git
